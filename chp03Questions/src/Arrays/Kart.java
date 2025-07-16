ca8f2ec44016598598efe349fd528c5560ef5ad0
burak <kandemir.burak94@gmail.com>
2025-07-16 11:37:53 +0300

    deneme

---
+++ chp03Questions/src/Arrays/Kart.java
@@ -0,0 +1,26 @@
+package Arrays;
+
+public class Kart {
+    private char deger;
+    private boolean tahmin = false;
+
+    public char getDeger() {
+        return deger;
+    }
+
+    public void setDeger(char deger) {
+        this.deger = deger;
+    }
+
+    public boolean isTahmin() {
+        return tahmin;
+    }
+
+    public void setTahmin(boolean tahmin) {
+        this.tahmin = tahmin;
+    }
+
+    public Kart(char deger) {
+        this.deger = deger;
+    }
+}
